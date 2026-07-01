4e48f394a1f4fb3a547d11231519ec704200cc53
Владислав Карасев <vladislavkarasev@MacBook-Pro-Vladislav.local>
2026-07-01 18:45:03 +0500

    debug: update query_metrics.py to test cAdvisor metrics

--- query_metrics.py
+++ query_metrics.py
@@ -2,14 +2,16 @@ import urllib.request
 import json
 
 queries = [
-    "kubevirt_vmi_storage_read_traffic_bytes_total",
-    "kubevirt_vmi_storage_read_iops_total"
+    'sum(rate(container_fs_reads_bytes_total{container="compute",pod=~"virt-launcher-.*"}[2m])) by (pod)',
+    'sum(rate(container_fs_writes_bytes_total{container="compute",pod=~"virt-launcher-.*"}[2m])) by (pod)',
+    'sum(rate(container_fs_reads_total{container="compute",pod=~"virt-launcher-.*"}[2m])) by (pod)',
+    'sum(rate(container_fs_writes_total{container="compute",pod=~"virt-launcher-.*"}[2m])) by (pod)'
 ]
 
 for q in queries:
     print(f"--- QUERY: {q} ---")
     try:
-        url = f"http://10.43.232.169:9090/api/v1/query?query={q}"
+        url = f"http://10.43.232.169:9090/api/v1/query?query={urllib.parse.quote(q)}"
         resp = urllib.request.urlopen(url, timeout=5)
         data = json.loads(resp.read().decode())
         results = data.get("data", {}).get("result", [])
